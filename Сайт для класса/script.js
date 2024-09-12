9f5845091957108c46916d3caec7bf2626e2a398
Daniar <122164247+Confogam@users.noreply.github.com>
2024-09-12 17:48:01 +0700

    Add files via upload

--- Сайт для класса/script.js
+++ Сайт для класса/script.js
@@ -14,3 +14,33 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+// Функция для отображения модального окна
+function showModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "block";
+}
+
+// Функция для закрытия модального окна
+function closeModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
+}
+
+// Показать модальное окно после загрузки страницы
+window.onload = function() {
+    showModal();
+};
+
+// Закрытие модального окна при нажатии на крестик
+var closeBtn = document.getElementsByClassName("close")[0];
+closeBtn.onclick = function() {
+    closeModal();
+};
+
+// Закрытие модального окна при нажатии вне его
+window.onclick = function(event) {
+    if (event.target == modal) {
+        closeModal();
+    }
+};
